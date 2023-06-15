0ca480e99017db091cd11663dea48869d43b52c9
Sandy <peacesandy04@gmail.com>
2023-06-15 03:04:23 -0700

    Started adding styling to the home page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,7 +2,29 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <div className='text-container'>
+      <div>
+        <h1>Seamless with Omingpt</h1>
+      </div>
+      <div>
+        <h4>Powered by chat gpt-4</h4>
+      </div>
+      <div>
+        <p>Experience the power of AI language models with OmniGPT.
+        Our chat platform provides seamless communication
+        across multiple channels.</p>
+      </div>
+      <div>
+        <p>
+        Try for free! 15-day trial, no credit card required.
+        </p>
+      </div>
+      <div>
+        <button>Show your free trial</button>
+      </div>
+      </div>
+    </div>
   )
 }
 
