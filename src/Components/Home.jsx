1078f2b3f26a547da5ea3068c734b1c35b22aa71
Sandy <peacesandy04@gmail.com>
2023-06-15 14:38:27 -0700

    added all images for home

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,6 +4,15 @@ import People from '../Assets/pexels-fauxels-3182812 Background Removed 1 (1).pn
 import Yellowlong from '../Assets/Rectangle 13060.png'
 import Whiterectangle from '../Assets/Rectangle 13061 (2).png'
 import Claphands from '../Assets/pexels-kindel-media-7688458 Background Removed 1.png'
+import Purplevector from '../Assets/Ellipse 94.png'
+import Purplearrow from '../Assets/Vector (4).png'
+import Greenvector from '../Assets/Ellipse 95.png'
+import Greenarrow from '../Assets/Vector (5).png'
+import Greenrectangle from '../Assets/Rectangle 13058 (1).png'
+import Purplerectangle from '../Assets/Rectangle 13057.png'
+import Techwoman from '../Assets/pexels-andrea-piacquadio-920382 Background Removed 1.png'
+import Yellowvector from '../Assets/Rectangle 13060.png'
+
 
 function Home() {
   return (
@@ -32,10 +41,20 @@ function Home() {
       <div className='images-left'>
           <img src={People} alt="" className='people-image'/> 
           <img src={Yellowlong} alt="" className='yellow-long' />
+          <img src={Purplevector} alt="" />
+          <img src={Purplearrow} alt="" />
       </div>
-      <div>
+      <div className='second-images'>
+        <img src={Greenvector} alt="" />
+          <img src={Greenarrow} alt="" />
+          <img src={Claphands} alt="" />
         <img src={Whiterectangle} alt="" />
-        <img src={Claphands} alt="" />
+      </div>
+      <div className='third-images'>
+        <img src={Greenrectangle} alt="" />
+        <img src={Purplerectangle} alt="" />
+        <img src={Techwoman} alt="" />
+        <img src={Yellowvector} alt="" />
       </div>
     </div>
   )
